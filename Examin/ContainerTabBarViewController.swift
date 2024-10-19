d8bac078f5d3c1b74d08980be1039eb09e975bd8
Ahmed Douss <ahmed.douss@edu.isetcom.tn>
2024-10-19 17:15:34 +0100

    Relaod Fav error

---
+++ Examin/ContainerTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContainerTabBarViewController.swift
+//  Examin
+//
+//  Created by ahmed douss on 19/10/2024.
+//
+
+import UIKit
+
+class ContainerTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
